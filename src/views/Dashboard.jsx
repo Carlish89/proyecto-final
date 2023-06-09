24cc2a03c142a8dcb87fb17cd90d6f8acbfd40a5
Carlos Cid <hsm.carlos89@gmail.com>
2023-06-09 17:49:16 -0400

    lasts corrections

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -7,16 +7,16 @@ import Cardb from '../components/Cardb'
 
 const Dashboard = () => {
 
-  const { favoritos, sumaAc, setPrecioAc, añadirProducto,deleteFavoritos } = useContext(Context)
+  const { favoritos, sumaAc, setPrecioAc, añadirProducto, deleteFavoritos } = useContext(Context)
 
   return (
     <div className='dashboard'>
       <div className='d-buttons'>
-     <Link to="/publicaciones"><Button className='button bg-primary'> <b>Mis publicaciones</b> </Button></Link> 
+        <Link to="/publicaciones"><Button className='button bg-primary'> <b>Mis publicaciones</b> </Button></Link>
         <h1 className='h1-t'> <b>Dashboard</b> </h1>
-       <Link to="/agregarp"><Button className='button bg-primary'><b>Agregar publicacion</b> </Button></Link> 
-      </div>
-      <div>
+        <Link to="/agregarp"><Button className='button bg-primary'><b>Agregar publicacion</b> </Button></Link>
+       </div>
+       <div>
         <h5> <b>Mis Favoritos</b> </h5>
       </div>
       <div className='d-galery'>
@@ -27,7 +27,7 @@ const Dashboard = () => {
               name={favoritos.nombre}
               price={favoritos.price}
               boton2={<Button variant="primary" onClick={() => { añadirProducto(favoritos.id); setPrecioAc(sumaAc(favoritos.id)) }} className='mx-2 bg-danger border border-0'>Añadir 🛒</Button>}
-              boton3={<Button onClick={()=>{deleteFavoritos(favoritos.id)}} className='mx-2 bg-warning border border-0'>Eliminar</Button>}
+              boton3={<Button onClick={() => { deleteFavoritos(favoritos.id) }} className='mx-2 bg-warning border border-0'>Eliminar</Button>}
             /></div>
         )}
       </div>
--- src/views/Publicaciones.jsx
+++ src/views/Publicaciones.jsx
@@ -3,17 +3,24 @@ import { useContext } from 'react'
 import { Context } from '../Context/Provider'
 import Cardb from '../components/Cardb'
 import { Button } from 'react-bootstrap'
-import "../styles/publicaciones.css"
+import "../styles/dashboard.css"
 import { Link } from 'react-router-dom'
 
 const Publicaciones = () => {
     const { productos, user, deleteProducto } = useContext(Context)
     const productosFilt = productos.filter(producto => producto.userid === user.id)
     return (
-        <div className='publicaciones'>
-            <h1 className='title'>Mis Publicaciones</h1>
+        <div className='dashboard'>
+            <div className='d-buttons'>
+                <Link to="/dashboard"><Button className='button bg-primary'><b>Mis Favoritos</b></Button></Link>
+                <h1 className='h1-t'> <b>Dashboard</b> </h1>
+                <Link to="/agregarp"><Button className='button bg-primary'><b>Agregar nueva publicacion</b></Button></Link>
+            </div>
+            <div>
+                <h5><b>Mis Publicaciones</b></h5>
+            </div>
             <div className='pub-galery'>
-                <div className='flex-container'>
+                <div className='d-galery'>
                     {productosFilt.map(productosFilt =>
                         <div key={productosFilt.id} className="card-g" style={{ width: '18rem' }}>
                             <Cardb
